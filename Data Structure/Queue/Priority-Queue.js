128010d01d6f85016634882dddd990b91645507f
me_shaon <shaon.cse81@gmail.com>
2017-04-07 00:44:27 +0600

    Priority Queue code added

---
+++ Data Structure/Queue/Priority-Queue.js
@@ -0,0 +1,54 @@
+/* This example and code taken from 'Learning Javascript Data Structures and Algorithms - Loiane Groner' */
+
+//PriorityQueue Class
+function PriorityQueue(){
+  var items = [];
+
+  function QueueElement(element, priority){
+    this.element = element;
+    this.priority = priority;
+  }
+
+  this.enqueue = function(element, priority){
+    var queueElement = new QueueElement(element, priority);
+
+    if(this.isEmpty()){
+      items.push(queueElement);
+    }
+    else {
+      var added = false;
+      for(var i = 0; i < items.length; i++){
+        if(queueElement.priority < items[i].priority){
+          items.splice(i,0,queueElement);
+          added = true;
+          break;
+        }
+      }
+      if(!added){
+        items.push(queueElement);
+      }
+    }
+
+  }
+
+  this.dequeue = function(){
+    return items.shift();
+  }
+
+  this.peek = function(){
+    return items[0];
+  }
+
+  this.isEmpty = function(){
+    return items.length === 0;
+  }
+}
+
+//Testing the Queue
+var queue = new PriorityQueue();
+
+queue.enqueue("John", 2);
+queue.enqueue("Jack", 1);
+queue.enqueue("Camila", 1);
+console.log(queue.isEmpty());
+console.log(queue.dequeue());
