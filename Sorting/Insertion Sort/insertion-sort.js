a5efc8fe2745cd1c320684c6f1bf4db9e5e9ca8e
me_shaon <shaon.cse81@gmail.com>
2017-04-26 00:37:21 +0600

    Insertion sort code comment and doc updated

--- Sorting/Insertion Sort/insertion-sort.js
+++ Sorting/Insertion Sort/insertion-sort.js
@@ -1,23 +1,22 @@
-//Insertion sort implementation
-function insertionSort(arr){
-    var len = arr.length;
+/*Insertion sort implementation in JavaScript */
+function insertionSort(arr) {
+	var len = arr.length;
 
-    for(var i=1; i<len; i++){
-       currentValue = arr[i];
-       j = i-1;
+	for (var i = 1; i < len; i++) {
+		currentValue = arr[i];
+		j = i - 1;
 
-       /* Move elements of arr[0..i-1], that are
-          greater than current_value, to one position ahead
-          of their current position */
-       while (j >= 0 && arr[j] > currentValue)
-       {
-           arr[j+1] = arr[j];
-           j--;
-       }
-       arr[j+1] = currentValue;
-    }
+		/* Move elements of arr[0..i-1], that are
+		   greater than current_value, to one position ahead
+		   of their current position */
+		while (j >= 0 && arr[j] > currentValue) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = currentValue;
+	}
 
-    return arr;
+	return arr;
 }
 
 /******************* Testing Selection sort algorithm *********************/
@@ -27,13 +26,13 @@ function insertionSort(arr){
  * Using Math.round() will give you a non-uniform distribution!
  */
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 var arr = [];
 
-for(var i=0;i<10;i++){ //initialize a random integer unsorted array
-    arr.push(getRandomInt(1, 100));
+for (var i = 0; i < 10; i++) { //initialize a random integer unsorted array
+	arr.push(getRandomInt(1, 100));
 }
 
 console.log("Unsorted array: ");
