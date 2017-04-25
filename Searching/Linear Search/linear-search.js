2eed549f79558a34cd8ed9858ded6a07099ac9db
me_shaon <shaon.cse81@gmail.com>
2017-04-26 00:17:37 +0600

    Linear search code comment updated

--- Searching/Linear Search/linear-search.js
+++ Searching/Linear Search/linear-search.js
@@ -1,17 +1,18 @@
-//Linear Search implementation
-function linearSearch(arr, item){
-    var len = arr.length;
-    for(var i=0; i<len; i++){
-        if(item === arr[i])
-        return true; //You can also return the index from here
-    }
+/* Linear Search implementation in JavaScript */
 
-    return false;
+function linearSearch(arr, item) {
+	var len = arr.length;
+	for (var i = 0; i < len; i++) {
+		if (item === arr[i])
+			return true; //You can also return the index from here
+	}
+
+	return false;
 }
 
 
-//Testing Linear Search implementation
-var hayStack = ['book','pencil','pen','note book','sharpener','rubber']
+/************ Testing Linear Search implementation ****************/
+var hayStack = ['book', 'pencil', 'pen', 'note book', 'sharpener', 'rubber']
 var needle = 'note book';
 
 console.log(linearSearch(hayStack, 'note book'));
