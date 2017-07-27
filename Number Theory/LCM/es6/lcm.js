67e1b47dfe97f3ed762048ef88ddc2532b5d620c
Ahmed Shamim <shaon.cse81@gmail.com>
2017-07-27 17:01:15 +0600

    LCM es6 code added

--- Number Theory/LCM/es6/lcm.js
+++ Number Theory/LCM/es6/lcm.js
@@ -1,4 +1,5 @@
 /* Least Commond Multiple (LCM) Finding in JavaScript */
+const gcd = require('./gcd');
 
 /*
   The basic formula of Calculating LCM of 'a' and 'b' is:
@@ -8,21 +9,4 @@
   by the gcd(a,b) before multiplication and reducing the possibility
   of 'integer overflow'
 */
-function lcm(a, b) {
-    return (a / gcd(a,b)) * b;
-}
-
-function gcd(a, b) {
-	while (true) {
-        var remainder = a%b;
-        if (remainder === 0) return b;
-
-        a = b;
-        b = remainder;
-    }
-}
-
-
-/************ Testing LCM ***************/
-console.log(lcm(5, 10));
-console.log(lcm(14, 35));
+module.exports.lcm = (a, b) => ((a / gcd(a,b)) * b);
---
+++ Number Theory/LCM/es6/test.js
@@ -0,0 +1,5 @@
+const { lcm } = require('./lcm');
+
+/************ Testing LCM ***************/
+console.log(lcm(5, 10));
+console.log(lcm(14, 35));
