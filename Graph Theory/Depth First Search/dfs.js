5f5c7be98055197edbe17d50cd79d398b59100a7
minhaz.raufoon <minhaz.raufoon@ipvisionsoft.com>
2017-12-11 16:31:59 +0600

    dfs added

---
+++ Graph Theory/Depth First Search/dfs.js
@@ -0,0 +1,42 @@
+var l = console.log;
+
+function DFSFindPath(graph, srcNode, destNode) {
+    var isVisited = Object.create(null),
+        isFound,
+        dfsPath,
+        nextNode;
+
+    dfsPath = [];
+    dfsTraverse(srcNode);
+
+    return {
+        distance: isFound ? dfsPath.length - 1 : Infinity,
+        path: isFound ? dfsPath : []
+    }
+
+    function dfsTraverse(node) {
+        isVisited[node] = true;
+        dfsPath.push(node);
+        isFound = node === destNode;
+        if (isFound) return;
+
+        for (var i = 0; i < graph[node].length; i++) {
+            nextNode = graph[node][i];
+            if (isVisited[nextNode]) continue;
+            dfsTraverse(nextNode);
+            if (isFound) return;
+        }
+    }
+}
+
+/* TESTING */
+var graph = {
+    1: [2, 3],
+    2: [1, 3, 4, 5],
+    3: [1, 2, 4],
+    4: [2, 3, 5],
+    5: [2, 4],
+}
+var srcNode = 1, destNode = 5;
+
+console.log(DFSFindPath(graph, srcNode, destNode));
